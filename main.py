fddf50146afdb64145aa92ba3ca947211d47c23f
Aashis Khanal <sraashis@gmail.com>
2022-02-04 10:24:57 -0500

    Start working.

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import subprocess
 import json
 import sys
 
+
 def main():
     doc = json.loads(sys.stdin.read())
     cmd = doc['input']['cmd'].split(' ')
-    #cmd = './plink --file toy --freq --out toy_analysis'.split(' ')
+    """cmd = './plink --file toy --freq --out toy_analysis'.split(' ')"""
     result = subprocess.check_output(cmd).decode()
     cmd = 'mv toy_analysis.* ' + doc['state']['outputDirectory']
-    subprocess.call(['mv','toy_analysis.*',doc['state']['outputDirectory']])
+    subprocess.call(['mv', 'toy_analysis.*', doc['state']['outputDirectory']])
 
     computation_output = {
         "output": {
@@ -19,8 +20,6 @@ def main():
 
     return json.dumps(computation_output)
 
-'''
 
-'''
 if __name__ == '__main__':
     main()
